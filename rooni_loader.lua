2a088ee3e1846cee1b8733f73e59bba67332384a
DarkTide93 <110002529+DarkTide93@users.noreply.github.com>
2022-09-30 16:53:29 -0400

    update scripts

--- rooni_loader.lua
+++ rooni_loader.lua
@@ -1,4 +1,4 @@
---loader for profile_test.gmr
+--you can share this code to customers
 GMR.SendRequestAndThen("raw.githubusercontent.com/DarkTide93/GMR-Rooni/main/profile_test.gmr", true,
         function(cipher)
             GMR.RunEncryptedScript(cipher)
