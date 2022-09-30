c6f2c3362090c29bbb474f0e7da2bbb207e1889c
DarkTide93 <110002529+DarkTide93@users.noreply.github.com>
2022-09-30 16:58:33 -0400

    update scripts

--- rooni_loader.lua
+++ rooni_loader.lua
@@ -4,3 +4,17 @@ GMR.SendRequestAndThen("raw.githubusercontent.com/DarkTide93/GMR-Rooni/main/prof
             GMR.RunEncryptedScript(cipher)
         end
 )
+--then you can send manually the loader to the customer,
+
+GMR.SendRequestAndThen("raw.githubusercontent.com/DarkTide93/GMR-Rooni/main/BF_PRO_ROONI.gmr", true,
+        function(cipher)
+            GMR.RunEncryptedScript(cipher)
+        end
+)
+GMR.SendRequestAndThen("raw.githubusercontent.com/DarkTide93/GMR-Rooni/main/BF_PRO_ROONI.gmr", true,
+        function(cipher)
+            GMR.RunEncryptedScript(cipher)
+        end
+)
+
+ok so another question what if i auth them is it for each script? "yes because you add the customer list at the top of each profile, look"
